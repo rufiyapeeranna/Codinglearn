a352c61a141d519785db5081406cb9e6167a9770
rufiyapeeranna <rufiyapeeranna1133@gmail.com>
2025-09-11 00:29:02 +0530

    convertArraytoArraylistusing-Streams

--- src/test/java/ConvertArraystoArrayList.java
+++ src/test/java/ConvertArraystoArrayList.java
@@ -1,13 +1,19 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ConvertArraystoArrayList {
 
     public static void main(String[] args) {
 
-        Integer a [] ={1,23,4,6,6};
+       int a [] ={1,23,4,6,6};
 
-       List<Integer> list=  Arrays.asList(a);
-        System.out.println(list);
+    // using streams
+
+     List<Integer> list1=  Arrays.stream(a).boxed().collect(Collectors.toList());
+        System.out.println(list1);
     }
 }
